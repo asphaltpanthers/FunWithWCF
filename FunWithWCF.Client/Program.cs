bc1b591885de383a2f418f8fee7119147715a56b
diser <diser@asphaltpanthers.com>
2016-06-23 21:13:01 -0400

    Add project files.

---
+++ FunWithWCF.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FunWithWCF.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating proxy for the Service
+            Service1Client client = new Service1Client();
+            Console.WriteLine(client.GetData(1));
+
+            var composite = client.GetDataUsingDataContract(new CompositeType
+            {
+                BoolValue = true,
+                StringValue = "test"
+            });
+            Console.WriteLine(composite.BoolValue);
+            Console.WriteLine(composite.StringValue);
+            Console.Read();
+        }
+    }
+}
